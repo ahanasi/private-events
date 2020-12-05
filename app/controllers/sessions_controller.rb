ecbbe240ef719ac96bd86b29196be5043896c433
ahanasi <ahanasi@seas.upenn.edu>
2020-12-04 23:45:52 -0800

    Create login layout and configure sessions#create route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:username])
+    user = User.find_by_username(session_params[:username])
     if user
       session[:user_id] = user.id
       redirect_to root_path, notice: "Logged In!"
     else
       flash.now[:alert] = "Invalid username"
+      render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged Out!"
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
+  post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy", as: "logout"
 end
