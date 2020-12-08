6c9adfadd307d7818075799b3eba69e1211ef1c9
ahanasi <ahanasi@seas.upenn.edu>
2020-12-07 18:44:02 -0800

    Modify user show page to list all user-created events

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_045244) do
+ActiveRecord::Schema.define(version: 2020_12_08_022717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
