5eccdbf1a024ef93eb49b9bedd560c67fe3a8244
ahanasi <ahanasi@seas.upenn.edu>
2020-12-09 20:14:07 -0800

    Add through table EventAttendings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_022717) do
+ActiveRecord::Schema.define(version: 2020_12_10_031008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "event_attendings", force: :cascade do |t|
+    t.bigint "event_id"
+    t.bigint "attendee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attendee_id"], name: "index_event_attendings_on_attendee_id"
+    t.index ["event_id"], name: "index_event_attendings_on_event_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
