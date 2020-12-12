9b945f9cd76e6561fe05cf8f415db52a1948a50a
ahanasi <ahanasi@seas.upenn.edu>
2020-12-12 13:03:24 -0800

    Create class methods for past and upcomign events, show on event index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :event_attendings
   has_many :attendees, through: :event_attendings
+
+  def self.past
+    where("date < ?", Date.current)
+  end
+
+  def self.upcoming
+    where("date >= ?", Date.current)
+  end
 end
